68859048c0a6f1fde2216b1385291f20b246a5ff
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-10-06 17:38:12 +0100

    mpi

--- .github/scripts/test.py
+++ .github/scripts/test.py
@@ -1,6 +1,7 @@
 import bempp.api
+from mpi4py import MPI
 import dolfinx
 
 grid = bempp.api.shapes.cube(h=0.5)
 
-mesh = dolfinx.UnitCubeMesh(2, 2, 2)
+mesh = dolfinx.UnitCubeMesh(MPI.COMM_WORLD, 2, 2, 2)
